fbc41714e9e6debf2cf7fde0e4207b1f55320b29
MariyaMoh <alshaibi199732@gmail.com>
2022-09-17 08:47:02 +0200

    MongoDB Connected

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const colors = require('colors');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
+connectDB();
 const app = express();
 
+app.use(express.json());
+app.use(express);
+
+// app.use('/api/users', require('./routes/userRoutes'));
 app.listen(port, () => console.log(`server started on post ${port}`));
